d2e6efdc5f4835c1e08f157502ef7a20df982c1a
Hernan Olivieri <hernan.olivieri@gmail.com>
2020-04-11 23:26:41 -0700

    Tabs

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TabsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-              physics: BouncingScrollPhysics(),
-              children: <Widget>[
-                  Container(color: Colors.red),
-                  Container(color: Colors.orange)
-              ],
-              )
+    return ChangeNotifierProvider(
+        create: (_) => new _NavegacionModel(),
+          child: Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+      ),
     );
   }
+}
+
+class _Navegacion extends StatelessWidget {
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+    return BottomNavigationBar(
+                            currentIndex: navegacionModel.paginaActual,
+                            onTap: (i) => navegacionModel.paginaActual = i,
+                            items: [
+                                  BottomNavigationBarItem(icon: Icon(Icons.account_balance), title: Text('Para ti')),
+                                  BottomNavigationBarItem(icon: Icon(Icons.public), title: Text('Encabezados'))
+                                  ]
+                            );
+  }
+}
+
+class _Paginas extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
+    return PageView(
+            controller: navegacionModel.pagecontroller,
+            //physics: BouncingScrollPhysics(), // para poder mover las pantallas
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+                Container(color: Colors.red),
+                Container(color: Colors.orange)
+            ],
+            );
+  }
+}
+
+class _NavegacionModel with ChangeNotifier {
+
+  PageController _pageController = new PageController( initialPage: 1);
+
+
+  int _paginaActual = 0;
+
+  int get paginaActual => _paginaActual;
+
+  set paginaActual( int valor ) {
+    _paginaActual = valor;
+    _pageController.animateToPage(valor, duration: Duration(milliseconds: 250), curve: Curves.easeInOut);
+    notifyListeners();
+  }
+
+  PageController get pagecontroller => this._pageController;
+
+
 }
